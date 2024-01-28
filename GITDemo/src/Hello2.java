629f9323497910b8c57ccc353e1fd6911b548253
sandip <sandippatil705@gmail.com>
2024-01-29 00:21:05 +0530

    added nw print ln line

--- GITDemo/src/Hello2.java
+++ GITDemo/src/Hello2.java
@@ -3,7 +3,8 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Hello2");
+ System.out.println("Hello444");
+ System.out.println("This is java language");
 	}
 
 }
